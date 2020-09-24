6624c5e4cda99ec60a4d269fe4f60e4372723ffb
Lars Hopman <l.k.hopman@gmail.com>
2020-09-24 16:00:42 +0200

    Remove trailing slashes for REST API

--- nopassword/urls.py
+++ nopassword/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 from nopassword import views
 
 urlpatterns = [
-    url(r'^login/$', views.LoginView.as_view(), name='login'),
-    url(r'^login/code/$', views.LoginCodeView.as_view(), name='login_code'),
-    url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
+    url(r'^login$', views.LoginView.as_view(), name='login'),
+    url(r'^login/code$', views.LoginCodeView.as_view(), name='login_code'),
+    url(r'^logout$', views.LogoutView.as_view(), name='logout'),
 ]
