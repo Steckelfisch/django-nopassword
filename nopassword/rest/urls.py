493c70b5acf57bb2b7e40b180b5ab2bc795c266a
Lars Hopman <l.k.hopman@gmail.com>
2020-09-29 09:09:29 +0200

    v.5.0.7 Remove deprecated re in url pattern

--- nopassword/rest/urls.py
+++ nopassword/rest/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 from nopassword.rest import views
 
 urlpatterns = [
-    url(r'^login/$', views.LoginView.as_view(), name='rest_login'),
-    url(r'^login/code/$', views.LoginCodeView.as_view(), name='rest_login_code'),
-    url(r'^logout/$', views.LogoutView.as_view(), name='rest_logout'),
+    url('login', views.LoginView.as_view(), name='rest_login'),
+    url('login/code', views.LoginCodeView.as_view(), name='rest_login_code'),
+    url('logout', views.LogoutView.as_view(), name='rest_logout'),
 ]
