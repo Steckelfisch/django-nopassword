a681254c2fade8374f17fbcadff45851469e6551
Lars Hopman <l.k.hopman@gmail.com>
2020-09-29 14:04:47 +0200

    Use correct order for URLs in auth

--- nopassword/rest/urls.py
+++ nopassword/rest/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 from nopassword.rest import views
 
 urlpatterns = [
-    url('login', views.LoginView.as_view(), name='rest_login'),
     url('login/code', views.LoginCodeView.as_view(), name='rest_login_code'),
+    url('login', views.LoginView.as_view(), name='rest_login'),
     url('logout', views.LogoutView.as_view(), name='rest_logout'),
 ]
