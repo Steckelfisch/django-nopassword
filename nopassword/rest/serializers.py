8c2bef7f846a88c1ceb9e876448bfbb4095cf54b
Lucho <luciano@tapp.cafe>
2022-06-08 16:09:25 +0200

    [HORECA-810] Integrate reCaptcha validation

--- nopassword/rest/serializers.py
+++ nopassword/rest/serializers.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import json
+from urllib.parse import urlencode
+from urllib.request import urlopen
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
@@ -7,15 +10,34 @@ from nopassword import forms
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
+    recaptcha_token = serializers.CharField()
     next = serializers.CharField(required=False, allow_null=True)
 
     form_class = forms.LoginForm
 
     def validate(self, data):
-        self.form = self.form_class(data=self.initial_data)
+        # Validate reCaptcha
+        URIReCaptcha = 'https://www.google.com/recaptcha/api/siteverify'
+        private_recaptcha = '6Lec01MgAAAAACNe3aYAbruNVTk3BaWP39rN5wsf'
+        recaptcha_token = data['recaptchaToken']
+        remote_ip = '127.0.0.1'
+        params = urlencode({
+            'secret': private_recaptcha,
+            'response': recaptcha_token,
+            'remote_ip': remote_ip,
+        })
 
-        if not self.form.is_valid():
-            raise serializers.ValidationError(self.form.errors)
+        data = urlopen(URIReCaptcha, params.encode('utf-8')).read()
+        result = json.loads(data)
+        success = result.get('success', None)
+
+        if not success:
+            raise serializers.ValidationError('Invalid reCaptcha')
+        else:
+            self.form = self.form_class(data=self.initial_data)
+
+            if not self.form.is_valid():
+                raise serializers.ValidationError(self.form.errors)
 
         return self.form.cleaned_data
 
