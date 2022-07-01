f5a9cc3e16d932f912678aae7ba208f015f570ca
Lucho <luciano@tapp.cafe>
2022-07-01 11:34:24 +0200

    [HORECA-814] Remove unused URLs

--- nopassword/urls.py
+++ nopassword/urls.py
@@ -1,9 +1,5 @@
 # -*- coding: utf8 -*-
 from django.conf.urls import url
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 from nopassword import views
 
@@ -11,6 +7,4 @@ urlpatterns = [
     url(r'^login$', views.LoginView.as_view(), name='login'),
     url(r'^login/code$', views.LoginCodeView.as_view(), name='login_code'),
     url(r'^logout$', views.LogoutView.as_view(), name='logout'),
-    url('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    url('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
